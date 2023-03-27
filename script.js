9a87c7e30ed27e2853f7956887d0b0d884e208a8
Derek Truong <derektruongdev@gmail.com>
2023-03-26 19:18:56 -0500

    dark mode working, improve scroll effect

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const cards = document.querySelectorAll(".featured-projects");
+const darkModeBTN = document.querySelector(".light-dark-btn");
+const sunMoonIMG = document.querySelector(".light-dark");
+const body = document.querySelector("body");
+//THEME
+darkModeBTN.addEventListener("click", changeTheme);
 
+let isDarkModeOn = false;
+
+function changeTheme(e) {
+  if (isDarkModeOn) {
+    sunMoonIMG.src = "assets/moon.png";
+    body.classList.remove("dark-theme");
+    isDarkModeOn = false;
+  } else {
+    sunMoonIMG.src = "assets/sun.png";
+    body.classList.add("dark-theme");
+    isDarkModeOn = true;
+  }
+}
+
+//SCROLL
 document.addEventListener("scroll", () => {
   const currentScroll = window.scrollY;
   //console.log({ currentScroll });
@@ -22,65 +42,77 @@ document.addEventListener("scroll", () => {
   }
   let active = 0;
 
-  if (isBetween(38, 42, scrollPercentage)) {
-    const fadeInOpacity = mapValue(scrollPercentage, 38, 42, 0, 100);
-    const movementIn = mapValue(scrollPercentage, 38, 42, 60, 50);
+  if (isBetween(38, 41, scrollPercentage)) {
+    const fadeInOpacity = mapValue(scrollPercentage, 38, 41, 0, 100);
+    const movementIn = mapValue(scrollPercentage, 38, 41, 60, 50);
     cards[0].style.opacity = `${(fadeInOpacity / 100) * 2}`;
     cards[0].style.top = `${movementIn}%`;
     cards[0].style.translate = `-50% -${movementIn}%`;
     active = 0;
   }
-  if (isBetween(45, 49, scrollPercentage)) {
-    const fadeOutOpacity = mapValue(scrollPercentage, 45, 49, 100, 0);
-    const movementOut = mapValue(scrollPercentage, 45, 49, 50, 40);
+  if (isBetween(47, 50, scrollPercentage)) {
+    const fadeOutOpacity = mapValue(scrollPercentage, 47, 50, 100, 0);
+    const movementOut = mapValue(scrollPercentage, 47, 50, 50, 40);
     cards[0].style.opacity = `${(fadeOutOpacity / 100) * 2}`;
     cards[0].style.top = `${movementOut}%`;
     cards[0].style.translate = `-50% -${movementOut}%`;
+    active = 0;
   }
 
-  if (isBetween(52, 56, scrollPercentage)) {
-    const fadeInOpacity = mapValue(scrollPercentage, 52, 56, 0, 100);
-    const movementIn = mapValue(scrollPercentage, 52, 56, 60, 50);
+  if (isBetween(53, 56, scrollPercentage)) {
+    const fadeInOpacity = mapValue(scrollPercentage, 53, 56, 0, 100);
+    const movementIn = mapValue(scrollPercentage, 53, 56, 60, 50);
     cards[1].style.opacity = `${(fadeInOpacity / 100) * 2}`;
     cards[1].style.top = `${movementIn}%`;
     cards[1].style.translate = `-50% -${movementIn}%`;
     active = 1;
   }
-  if (isBetween(59, 63, scrollPercentage)) {
-    const fadeOutOpacity = mapValue(scrollPercentage, 59, 63, 100, 0);
-    const movementOut = mapValue(scrollPercentage, 59, 63, 50, 40);
+  if (isBetween(56, 62, scrollPercentage)) {
+    active = 1;
+  }
+  if (isBetween(62, 65, scrollPercentage)) {
+    const fadeOutOpacity = mapValue(scrollPercentage, 62, 65, 100, 0);
+    const movementOut = mapValue(scrollPercentage, 62, 65, 50, 40);
     cards[1].style.opacity = `${(fadeOutOpacity / 100) * 2}`;
     cards[1].style.top = `${movementOut}%`;
     cards[1].style.translate = `-50% -${movementOut}%`;
+    active = 1;
   }
 
-  if (isBetween(66, 70, scrollPercentage)) {
-    const fadeInOpacity = mapValue(scrollPercentage, 66, 70, 0, 100);
-    const movementIn = mapValue(scrollPercentage, 66, 70, 60, 50);
+  if (isBetween(68, 71, scrollPercentage)) {
+    const fadeInOpacity = mapValue(scrollPercentage, 68, 71, 0, 100);
+    const movementIn = mapValue(scrollPercentage, 68, 71, 60, 50);
     cards[2].style.opacity = `${(fadeInOpacity / 100) * 2}`;
     cards[2].style.top = `${movementIn}%`;
     cards[2].style.translate = `-50% -${movementIn}%`;
     active = 2;
   }
-  if (isBetween(73, 77, scrollPercentage)) {
-    const fadeOutOpacity = mapValue(scrollPercentage, 73, 77, 100, 0);
-    const movementOut = mapValue(scrollPercentage, 73, 77, 50, 40);
+  if (isBetween(71, 77, scrollPercentage)) {
+    active = 2;
+  }
+  if (isBetween(77, 80, scrollPercentage)) {
+    const fadeOutOpacity = mapValue(scrollPercentage, 77, 80, 100, 0);
+    const movementOut = mapValue(scrollPercentage, 77, 80, 50, 40);
     cards[2].style.opacity = `${(fadeOutOpacity / 100) * 2}`;
     cards[2].style.top = `${movementOut}%`;
     cards[2].style.translate = `-50% -${movementOut}%`;
+    active = 2;
+  }
+  if (isBetween(80, 83, scrollPercentage)) {
+    active = 2;
   }
 
-  if (isBetween(80, 84, scrollPercentage)) {
-    const fadeInOpacity = mapValue(scrollPercentage, 80, 84, 0, 100);
-    const movementIn = mapValue(scrollPercentage, 80, 84, 60, 50);
+  if (isBetween(83, 86, scrollPercentage)) {
+    const fadeInOpacity = mapValue(scrollPercentage, 83, 86, 0, 100);
+    const movementIn = mapValue(scrollPercentage, 83, 86, 60, 50);
     cards[3].style.opacity = `${(fadeInOpacity / 100) * 2}`;
     cards[3].style.top = `${movementIn}%`;
     cards[3].style.translate = `-50% -${movementIn}%`;
     active = 3;
   }
-  if (isBetween(87, 91, scrollPercentage)) {
-    const fadeOutOpacity = mapValue(scrollPercentage, 87, 91, 100, 0);
-    const movementOut = mapValue(scrollPercentage, 87, 91, 50, 40);
+  if (isBetween(92, 100, scrollPercentage)) {
+    const fadeOutOpacity = mapValue(scrollPercentage, 92, 95, 100, 0);
+    const movementOut = mapValue(scrollPercentage, 92, 95, 50, 40);
     cards[3].style.opacity = `${(fadeOutOpacity / 100) * 2}`;
     cards[3].style.top = `${movementOut}%`;
     cards[3].style.translate = `-50% -${movementOut}%`;
@@ -91,6 +123,7 @@ document.addEventListener("scroll", () => {
     }
   });
 });
+
 function mapValue(value, start1, end1, start2, end2) {
   return Math.round(
     ((value - start1) / (end1 - start1)) * (end2 - start2) + start2
@@ -98,33 +131,3 @@ function mapValue(value, start1, end1, start2, end2) {
 }
 
 const isBetween = (start, end, value) => value >= start && value <= end;
-
-// THEMES
-const themeButton = document.querySelector("#theme");
-
-let currentTheme = localStorage.getItem("theme");
-
-if (currentTheme == "dark") {
-  document.body.classList.add("dark-theme");
-  themeButton.innerText = "🌞";
-} else {
-  themeButton.innerText = "🌚";
-  currentTheme = "light";
-}
-
-function themeMode() {
-  document.body.classList.toggle("dark-theme");
-
-  if (currentTheme == "dark") {
-    currentTheme = "light";
-    themeButton.innerText = "🌚";
-  } else {
-    currentTheme = "dark";
-    themeButton.innerText = "🌞";
-  }
-
-  localStorage.setItem("theme", currentTheme);
-}
-
-themeButton.addEventListener("click", themeMode);
-document.documentElement.style.setProperty("--background-color", "#000");
